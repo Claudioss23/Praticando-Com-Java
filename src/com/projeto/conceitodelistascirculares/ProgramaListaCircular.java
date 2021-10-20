f789641d72ca512a808f49372770f117c52ac3b2
Claudioss23 <souza.claudiof23@gmail.com>
2021-10-20 13:53:55 -0300

    Conceito de Listas Circulares

---
+++ src/com/projeto/conceitodelistascirculares/ProgramaListaCircular.java
@@ -0,0 +1,31 @@
+package com.projeto.conceitodelistascirculares;
+
+public class ProgramaListaCircular {
+    public static void main(String[] args) {
+
+        ListaCircular<String> minhaListaCircular = new ListaCircular<>();
+
+        minhaListaCircular.add("c0");
+        System.out.println(minhaListaCircular);
+
+        minhaListaCircular.remove(0);
+        System.out.println(minhaListaCircular);
+
+        minhaListaCircular.add("c1");
+        System.out.println(minhaListaCircular);
+
+        minhaListaCircular.add("c2");
+        minhaListaCircular.add("c3");
+        System.out.println(minhaListaCircular + "\n");
+
+        System.out.println(minhaListaCircular.get(0));
+        System.out.println(minhaListaCircular.get(1));
+        System.out.println(minhaListaCircular.get(2));
+        System.out.println(minhaListaCircular.get(3));
+        System.out.println(minhaListaCircular.get(4) + "\n");
+
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("Índice " + i + "= " + minhaListaCircular.get(i));
+        }
+    }
+}
